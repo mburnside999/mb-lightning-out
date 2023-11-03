8d5bd1874f1ae99a33f61db42b0834149be4aa60
Mike Burnside <mburnside@salesforce.com>
2023-11-03 15:22:00 +1100

    images

--- app.py
+++ app.py
@@ -3,13 +3,16 @@ from simple_salesforce import Salesforce
 import pandas as pd
 import os
 from sklearn.linear_model import LinearRegression
-
-
 from sklearn.model_selection import train_test_split
 from sklearn.metrics import mean_squared_error, r2_score
+import seaborn as sns
 from joblib import dump, load
 import numpy as np
-from simple_salesforce import Salesforce
+import matplotlib
+matplotlib.use('Agg')
+import matplotlib.pyplot as plt
+from io import BytesIO
+import base64
 
 
 app = Flask(__name__)
@@ -24,9 +27,11 @@ SALESFORCE_CLIENT_SECRET = '0BED54F5CC3E4B25160AF24AB3137D272C017BB6DC4135AA54F0
 
 app = Flask(__name__)
 
+
+print ('connecting')
 # Connect to Salesforce
 sf = Salesforce(username=SALESFORCE_USERNAME, password=SALESFORCE_PASSWORD, security_token=SALESFORCE_SECURITY_TOKEN)
-
+print ('connected')
 # Load the model later for predictions
 loaded_model = load('wines_model310.joblib')
 print (loaded_model)
@@ -36,8 +41,38 @@ def list_wines():
     # Query Salesforce for a list of wines
     query = "SELECT Id, Name,alcohol__c,chlorides__c,citric_acid__c,density__c,fixed_acidity__c,free_sulfur_dioxide__c,pH__c,residual_sugar__c,sulphates__c,total_sulfur_dioxide__c,volatile_acidity__c,quality__c FROM Wines__c order by Name"
     wines = sf.query_all(query)['records']
+    print (type(wines))
 
-    return render_template('wine.html', wines=wines)
+    img = BytesIO()
+    #convert to df
+    data = pd.DataFrame(wines)
+    #drop useless cols
+    data.drop(['attributes','Id','Name'],axis=1,inplace=True)
+    print (data.head())
+    #set up correlation
+    correlation = data.corr()
+    plt.figure(figsize=(12,12))
+    sns.heatmap(correlation, cbar=True, square=True, fmt = '.1f', annot = True, annot_kws={'size':8}, cmap = 'Blues')
+    plt.savefig(img, format='png')
+    plt.close()
+    img.seek(0)
+    plot_url = base64.b64encode(img.getvalue()).decode('utf8')
+    return render_template('wine.html',plot_url=plot_url,wines=wines)
+
+
+@app.route('/plot')
+def plotter():
+    img = BytesIO()
+    wine_dataset = pd.read_csv('Wines.csv')
+    correlation = wine_dataset.corr()
+    plt.figure(figsize=(10,10))
+    sns.heatmap(correlation, cbar=True, square=True, fmt = '.1f', annot = True, annot_kws={'size':8}, cmap = 'Blues')
+    plt.savefig(img, format='png')
+    plt.close()
+    img.seek(0)
+    plot_url = base64.b64encode(img.getvalue()).decode('utf8')
+    
+    return render_template('plot.html', plot_url=plot_url)
 
 @app.route('/wines', methods=['POST'])  
 def predict_wines():
@@ -75,10 +110,13 @@ def predict_wines():
 
 @app.route('/linear_regression')
 def linear_regression():
+    
+    img = BytesIO()
+    
     # Query Salesforce for data
-    query = "SELECT AnnualRevenue, NumberOfEmployees FROM Account WHERE AnnualRevenue !=NULL AND NumberOfEmployees !=NULL"
+    query = "SELECT AnnualRevenue, NumberOfEmployees FROM Account WHERE AnnualRevenue !=NULL AND NumberOfEmployees !=NULL limit 10"
     results = sf.query_all(query)['records']
-
+    
 # Convert data to a Pandas DataFrame
     data = pd.DataFrame(results)
     print (data)
@@ -103,12 +141,23 @@ def linear_regression():
 
     print(f"Mean Squared Error: {mse:.2f}")
     print(f"R-squared: {r2:.2f}")
+
+    data['NumberOfEmployees'].plot(kind='box')
+
+
+    plt.savefig(img, format='png')
+    plt.close()
+    img.seek(0)
+    plot_url = base64.b64encode(img.getvalue()).decode('utf8')
+
+
+
     # Create and train a linear regression model (as in the previous code)
     # ...
     # Replace this with your model creation and evaluation code.
 
     # Render the results template with the model's results
-    return render_template('results.html', mse=mse, r2=r2)
+    return render_template('results.html', plot_url=plot_url,mse=mse, r2=r2)
 
 
 port = int(os.environ.get('PORT', 5002))
